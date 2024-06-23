36ae6f942cbc85e1ac674e8e383d83882a192553
Yash Gaur <132750672+The-Yash-Gaur@users.noreply.github.com>
2024-06-23 23:39:41 +0530

    Add files via upload

---
+++ Network_Sniffer.py
@@ -0,0 +1,15 @@
+from scapy.all import *
+from scapy.layers.l2 import Ether
+
+sniffer = socket.socket(socket.AF_PACKET , socket.SOCK_RAW , socket.ntohs(3))
+
+interface = "eth0"
+sniffer.bind((interface, 0))
+
+try:
+    while True:
+        raw_data , addr = sniffer.recvfrom(65535)
+        packet=Ether(raw_data)
+        print(packet.summary())
+except KeyboardInterrupt:
+    sniffer.close()
